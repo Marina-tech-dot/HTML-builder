e70be44cc3d8f113a96e78e71b0a3bc987dd41d4
Marina Hudkova <85888955+Marina-tech-dot@users.noreply.github.com>
2023-05-09 00:14:49 +0200

    feat: task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,33 @@
+const fs = require('node:fs/promises');
+const path = require('path');
+const { mkdir, readdir } = require('node:fs/promises');
 
+const pathToFiles = path.join(__dirname, 'files');
+const pathToCopy = path.join(__dirname, 'filesCopy');
+
+  
+(async() => {
+  try {
+    await mkdir(pathToCopy, {recursive: true});
+    copy()
+  } catch (err) {
+    console.log(err, 'Copying is not accessible')
+  }
+})()
+
+
+
+const copy = async () => {
+  const curContent = await readdir(pathToFiles);
+  
+  curContent.forEach(async(item) => {
+    let basicPath = path.join(__dirname, 'files', item)
+    let pathToCopy = path.join(__dirname, 'filesCopy', item)
+
+    try {
+      await fs.copyFile(basicPath, pathToCopy)
+    } catch (err) {
+      console.log(`The files have not been copied`)
+    }
+  });
+}
