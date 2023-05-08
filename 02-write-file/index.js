c4edbbd5edba0c9457731983f23c3ace799f1e3c
Marina Hudkova <85888955+Marina-tech-dot@users.noreply.github.com>
2023-05-09 00:11:54 +0200

    feat: task 2

---
+++ 02-write-file/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+const { stdin, stdout } = require('process')
+
+fs.open('userNote.txt', 'w', err => {
+  if (err) throw new err;
+})
+
+stdin.write(`So, tell me whatever you want... \n`)
+
+  stdin.on('data', (data) => {
+    if (data.toString().trim() === 'exit') process.exit();
+
+    fs.appendFile('userNote.txt', data, err => {
+  if (err) throw new err;
+  })
+});
+
+process.on("SIGINT", function () {
+  process.exit();
+});
+
+process.on('exit', () => stdout.write(`Your thoughts are written in the userNote.txt. You can find it in the current folder. \n`))
+
