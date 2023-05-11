40f90ec8938b099cb35c34d28322cc2e19a2c467
Marina Hudkova <85888955+Marina-tech-dot@users.noreply.github.com>
2023-05-11 10:01:48 +0200

    fix: 6 task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,128 +1,137 @@
-const FS = require("fs");
-const fs = require("node:fs/promises");
-const path = require("path");
-const {  mkdir, readdir } = require("node:fs/promises");
-
-const root = path.join(__dirname, "project-dist");
+const FS = require('fs');
+const fs = require('node:fs/promises');
+const path = require('path');
 
 
-const copyFolder = async (name) => {
-  const pathToSource = path.join(__dirname, name); // .../assets
-  const pathToCopy = path.join(root, name); // .. project-dist/assets
-  await mkdir(pathToCopy, { recursive: true });
-
-  const curContent = await readdir(pathToSource, {
-    withFileTypes: true,
+const find = (arr) => {
+  let result = [];
+  arr.forEach((element) => {
+    const regex = /{{(.*?)}}/g;
+    let match;
+    while ((match = regex.exec(element))) {
+      result.push(match[1].replace(/[{}]/g, ''));
+    }
   });
-
-  curContent.forEach(async (item) => {
-    copyFile(item, pathToCopy, pathToSource);
-  })
+  return result;
 };
 
-const copyFile = async(file, pathToCopy, pathToSource) => {
-  if (typeof file === 'string') {
-        let basicPath = path.join(pathToSource, file);
-        let copyPath = path.join(pathToCopy, file);
-        try {
-          await fs.copyFile(copyPath, basicPath);
-        } catch (err) {
-          console.log(`The files have not been copied`);
-        }
-        return
+const insert = (arr, str1, str2) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].includes(str1)) {
+      arr[i] = arr[i].replace(str1, str2);
     }
-
-  if (file.isFile() === false) {
-      try {
-        const pathToSubFolder = path.join(pathToSource, file.name)
-        const pathToFile = path.join(pathToCopy, file.name);
-        await mkdir(pathToFile, { recursive: true });
-        const folder = await readdir(pathToSubFolder);
-        folder.forEach((file) => {
-          copyFile(file, pathToSubFolder, pathToFile);
-        })
-      } catch (err) {
-        console.log('Err with subfolder copying')
-      }
-    } 
-}
+  }
+};
 
 const cssBundle = async () => {
-  const pathToCSS = path.join(__dirname, 'styles')
-  const pathToBundleCss = path.join(root, 'style.css' );
+  const pathToCSS = path.join(__dirname, 'styles');
+  const root = path.join(__dirname, 'project-dist');
+  const pathToBundleCss = path.join(root, 'style.css');
+  const stylesArr = [];
 
   try {
-    const cssFiles = await readdir(pathToCSS, {
+    const cssFiles = await fs.readdir(pathToCSS, {
       withFileTypes: true,
     });
 
-    let styles = ''
-
-    cssFiles.forEach((file) => {
+    cssFiles.forEach(async (file) => {
       const pathToFile = path.join(pathToCSS, file.name);
-      const ext = path.extname(pathToFile)
+      const ext = path.extname(pathToFile);
 
-      if (ext === ".css" && file.isFile()) {
-        const stream = FS.readFileSync(pathToFile, "utf-8");
-        styles += stream
-      }
-  })
-  
-      FS.writeFile(pathToBundleCss, styles, { flag: "w+" }, (err) => {
-        if (err) throw err;
-      });
-  } catch (err) {
-    console.log(err, 'Copying is not accessible')
-  }
-}
+      if (file.isFile()) {
+        if (ext === '.css' && file.isFile()) {
+          const stream = FS.createReadStream(pathToFile, 'utf-8');
+          stream.on('data', (chunk) => stylesArr.push(chunk));
+          stream.on('end', () => {
+            fs.writeFile(pathToBundleCss, '', (err) => {
+              if (err) throw err;
+            });
 
-const htmlTemplates = async () => {
-  const pathToFolder = path.dirname(__filename);
-  const pathTempl = path.join(pathToFolder, "template.html");
-  const pathToTemplates = path.join(__dirname, 'components');
-  const pathToNewHTML = path.join(root, 'index.html' );
-
-  try {
-    let HTMLFiles = await readdir(pathToTemplates);
-    HTMLFiles = HTMLFiles.map(
-      (file) => file.match(/([\w,\s-]+)\.[A-Za-z]{3}/)[1]
-    );
-
-      let newHTML = FS.readFileSync(pathTempl, "utf8");
-
-        HTMLFiles.forEach((templName) => {
-          const pathToTemplName = path.join(
-            pathToTemplates,
-            templName + ".html"
-          );
-
-            let templText = FS.readFileSync(
-              pathToTemplName,
-              "utf-8",
-            );
-
-            newHTML = newHTML.replace(`{{${templName}}}`, templText);
-
+            stylesArr.forEach((item) => {
+              fs.appendFile(pathToBundleCss, item, (err) => {
+                if (err) throw err;
+              });
+            });
           });
-
-
-        FS.writeFile(pathToNewHTML, newHTML, { flag: "w+" }, (err) => { 
-        if (err) throw err;
-      });
-  } catch(err) {
-    console.log(err, 'Problems with html templates')
+        }
+      }
+    });
+  } catch (err) {
+    console.log(err, 'Copying is not accessible');
   }
+};
+
+const copy = async (pathToFiles, pathToCopy) => {
+  try {
+    await fs.mkdir(pathToCopy, { recursive: true });
+    const files = await fs.readdir(pathToFiles);
+
+    for (const file of files) {
+      const pathToFile = path.join(pathToFiles, file);
+      const pathToCopyFile = path.join(pathToCopy, file);
+  
+      const fileStat = await fs.stat(pathToFile);
+  
+      if (fileStat.isDirectory()) {
+        await copy(pathToFile, pathToCopyFile);
+      } else {
+        await fs.copyFile(pathToFile, pathToCopyFile);
+      }
+    }
+  } catch (err) {
+    console.log('The files have not been copied');
+  }
+};
+
+
+
+async function dist() {
+  const templateContent = [];
+  const root = path.join(__dirname, 'project-dist');
+  
+
+  fs.mkdir(root, { recursive: true }, (error) => {
+    if (error) {
+      console.error(error, 'Creating mkdir');
+    }
+  });
+
+  const templatePath = path.join(__dirname, 'template.html');
+  const readableStream = FS.createReadStream(templatePath, 'utf-8');
+  readableStream.on('data', chunk => templateContent.push(chunk));
+  readableStream.on('end', () => {
+    const tags = find(templateContent);
+    tags.forEach(async (item) => {
+      const filePath = path.join(__dirname, 'components', `${item}.html`);
+    
+      try {
+        const tagContent = await fs.readFile(filePath, 'utf-8');
+        insert(templateContent, `{{${item}}}`, tagContent);
+    
+        const allProcessed = tags.every(async (item) => {
+          const newFilePath = path.join(__dirname, 'components', `${item}.html`);
+          const compareContent = await fs.readFile(newFilePath, 'utf8');
+          return compareContent.trim() === tagContent.trim();
+        });
+    
+        if (allProcessed) {
+          await fs.writeFile(
+            path.join(root, 'index.html'),
+            templateContent.join(''),
+          );
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    });
+    
+  });
+
+  cssBundle();
+
+  const pathToAssets = path.join(__dirname, 'assets');
+  const pathToCopyAssets = path.join(__dirname, 'project-dist','assets');
+  copy(pathToAssets, pathToCopyAssets);
 }
 
-
-
-(async () => {
-  await mkdir(root, { recursive: true });
-  try {
-    copyFolder('assets')
-    cssBundle()
-    htmlTemplates()
-  } catch (err) {
-    console.log(err, "Copying is not accessible");
-  }
-})();
+dist();
