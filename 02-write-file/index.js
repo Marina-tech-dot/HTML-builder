248210c0edf420042760cbe8f20a953206e8a3ae
Marina Hudkova <85888955+Marina-tech-dot@users.noreply.github.com>
2023-05-11 07:19:38 +0200

    fix: task 2

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,23 +1,22 @@
-const fs = require('fs')
-const { stdin, stdout } = require('process')
+const fs = require('fs');
+const { stdin, stdout } = require('process');
 
 fs.open('userNote.txt', 'w', err => {
   if (err) throw new err;
-})
-
-stdin.write(`So, tell me whatever you want... \n`)
-
-  stdin.on('data', (data) => {
-    if (data.toString().trim() === 'exit') process.exit();
-
-    fs.appendFile('userNote.txt', data, err => {
-  if (err) throw new err;
-  })
 });
 
-process.on("SIGINT", function () {
+console.log('So, tell me whatever you want... \n');
+
+stdin.on('data', (data) => {
+  if (data.toString().trim() === 'exit') process.exit();
+
+  fs.appendFile('userNote.txt', data, err => {
+    if (err) throw new err;
+  });
+});
+
+process.on('SIGINT', function () {
   process.exit();
 });
 
-process.on('exit', () => stdout.write(`Your thoughts are written in the userNote.txt. You can find it in the current folder. \n`))
-
+process.on('exit', () => stdout.write('Your thoughts are written in the userNote.txt. You can find it in the current folder. \n'));
